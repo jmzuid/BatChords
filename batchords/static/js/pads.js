ef175db104d3c5ce3a05b527ef624473ed3268af
Daniel Burke <burkdan@umich.edu>
2018-02-22 00:01:13 -0500

    got api to work with stop and play buttons

---
+++ batchords/static/js/pads.js
@@ -0,0 +1,22 @@
+var container = document.getElementById('embed-container');
+var embed = new Flat.Embed(container, {
+    score: '5a87d33f265c44553918dd0a',
+    embedParams: {
+      appId: '5a8e18e1a2f0e03339907c31'
+      // appId: 'localhost'
+    }
+});
+
+play_score.onclick = function(){
+    embed.play().then(function () {
+        // The score is playing
+    });
+    console.log("test");
+};
+
+stop_score.onclick = function(){
+    embed.stop().then(function () {
+        // The score is playing
+    });
+    console.log("test");
+};
