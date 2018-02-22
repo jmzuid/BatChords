700e90b8b8d26d38e7a502fc5133308428da418c
Daniel Burke <burkdan@umich.edu>
2018-02-22 05:06:13 -0500

    added color change when buttons clicked/pads pressed

--- batchords/static/js/keyboards.js
+++ batchords/static/js/keyboards.js
@@ -43,6 +43,15 @@ function onMIDIMessage(message) {
     AB1 = document.getElementById("AB1");
     B1 = document.getElementById("B1");
 
+    play_s = document.getElementById("play_score");
+    stop_s = document.getElementById("stop_score");
+    seek_ml = document.getElementById("seek_m_left");
+    seek_mr = document.getElementById("seek_m_right");
+    pause_s = document.getElementById("pause_score");
+    edit_s = document.getElementById("edit_score");
+    seek_nl = document.getElementById("seek_note_left");
+    seek_nr = document.getElementById("seek_note_right");
+
     if (data[2] != 0) { // this is the start signal
         if (data[1] == 48) {
             C1.style.backgroundColor = "rgb(100, 140, 190)";
@@ -86,27 +95,35 @@ function onMIDIMessage(message) {
         //Programming for the pads
         if (data[1] == 100) {
             play_score.click();
+            play_s.style.backgroundColor = "black";
         }
         if (data[1] == 101) {
             stop_score.click();
+            stop_s.style.backgroundColor = "black";
         }
         if (data[1] == 102) {
             seek_m_left.click();
+            seek_ml.style.backgroundColor = "black";
         }
         if (data[1] == 103) {
             seek_m_right.click();
+            seek_mr.style.backgroundColor = "black";
         }
         if (data[1] == 104) {
             pause_score.click();
+            pause_s.style.backgroundColor = "black";
         }
         if (data[1] == 105) {
             edit_score.click();
+            edit_s.style.backgroundColor = "black";
         }
         if (data[1] == 106) {
             seek_note_left.click();
+            seek_nl.style.backgroundColor = "black";
         }
         if (data[1] == 107) {
             seek_note_right.click();
+            seek_nr.style.backgroundColor = "black";
         }
 
 
@@ -147,6 +164,34 @@ function onMIDIMessage(message) {
         }
         if (data[1] == 59) {
             B1.style.backgroundColor = "white";
+        }
+
+
+
+        //Programming for the pads
+        if (data[1] == 100) {
+            play_s.style.backgroundColor = "#4CAF50";
+        }
+        if (data[1] == 101) {
+            stop_s.style.backgroundColor = "#4CAF50";
+        }
+        if (data[1] == 102) {
+            seek_ml.style.backgroundColor = "#4CAF50";
+        }
+        if (data[1] == 103) {
+            seek_mr.style.backgroundColor = "#4CAF50";
+        }
+        if (data[1] == 104) {
+            pause_s.style.backgroundColor = "#4CAF50";
+        }
+        if (data[1] == 105) {
+            edit_s.style.backgroundColor = "#4CAF50";
+        }
+        if (data[1] == 106) {
+            seek_nl.style.backgroundColor = "#4CAF50";
+        }
+        if (data[1] == 107) {
+            seek_nr.style.backgroundColor = "#4CAF50";
         }
     }
 }
