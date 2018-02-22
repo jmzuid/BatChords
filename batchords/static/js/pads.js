4f95a431d9ded2f7e98e78a7dfc01b1a51096424
Caroline Saab <carolinesaab@0587390940.wireless.umich.net>
2018-02-22 01:24:41 -0500

    Nest getCursor and setCursor. Doesn't work.

--- batchords/static/js/pads.js
+++ batchords/static/js/pads.js
@@ -1,8 +1,8 @@
 var container = document.getElementById('embed-container');
 var embed = new Flat.Embed(container, {
-    score: '5a87d33f265c44553918dd0a',
+    score: '5a88bca1deb7e53deddac504',
     embedParams: {
-      appId: '5a8e18e1a2f0e03339907c31'
+      appId: '5a8cec39dcfa9b10925941a0'
       // appId: 'localhost'
     }
 });
@@ -11,12 +11,38 @@ play_score.onclick = function(){
     embed.play().then(function () {
         // The score is playing
     });
-    console.log("test");
+    console.log("Playing Score");
 };
 
 stop_score.onclick = function(){
     embed.stop().then(function () {
-        // The score is playing
+        // The score is not playing
     });
-    console.log("test");
+    console.log("Stopping score");
+};
+
+seek_right.onclick = function() {
+    embed.getCursorPosition().then(function (position) {
+  // position: {
+  //     "partIdx": 0,
+  //     "staffIdx": 1,
+  //     "voiceIdx": 0,
+  //     "measureIdx": 2,
+  //     "noteIdx": 1
+  // }
+        console.log(position.staffIdx);
+        embed.setCursorPosition({
+          partIdx: position.partIdx,
+          staffIdx: position.staffIdx+1,
+          voiceIdx: position.voiceIdx,
+          measureIdx: position.measureIdx,
+          noteIdx: position.noteIdx
+        }).then(function (position) {
+          console.log("Position set")
+        });
+
+
+
+  });
+
 };
